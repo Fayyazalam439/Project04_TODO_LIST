ff863ccae65f78f22298606e11b838d722057796
Fayyazalam439 <160386224+Fayyazalam439@users.noreply.github.com>
2024-05-23 00:41:48 +0500

    Created Project04_TODO_LIST

---
+++ main.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addNewtask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What you want to add in your Todos?"
+        },
+        {
+            name: "addMore",
+            type: "input",
+            message: "Do you want to add more?",
+            default: "yes/No"
+        },
+        {
+            name: "ProjectEnd",
+            type: "input",
+            message: "Congratulation Project Todo is Completed.",
+        }
+    ]);
+    todos.push(addNewtask.todo);
+    condition = addNewtask.addMore;
+    condition = addNewtask.ProjectEnd;
+    console.log(`${todos}`);
+}
+;
